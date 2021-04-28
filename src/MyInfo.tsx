79eb07ead414f8b71054776db069e675dd2fc35d
Morris Boers <m.boers1@student.tue.nl>
2021-04-28 13:10:25 +0200

    Removed some unnessary files

---
+++ src/MyInfo.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+      <div>
+        <h1>Morris Boers</h1>
+        <p>Hello, my name is WillyDeWereldleier and I like to program</p>
+        <ul>
+          <li>Canada</li>
+          <li>Icelandasdfd</li>
+          <li>New zealand</li>
+        </ul>
+      </div>
+    )
+}
+
+export default MyInfo
