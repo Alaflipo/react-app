f201fbaf42af4ba96aa2876d8d44b8a17825786b
Morris Boers <m.boers1@student.tue.nl>
2021-04-28 13:33:16 +0200

    started making the TODO app

---
+++ src/components/Main.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+function Main() {
+    return (
+        <main>
+            <p>TODO: </p>
+            <input type="checkbox" id="c1" checked></input>
+            <label htmlFor="c1">Item1</label>
+            <br></br>
+            <input type="checkbox" id="c2" checked></input>
+            <label htmlFor="c2">Item2</label>
+            <br></br>
+            <input type="checkbox" id="c3" checked></input>
+            <label htmlFor="c3">Item3</label>
+        </main>
+    )
+
+}
+
+export default Main
